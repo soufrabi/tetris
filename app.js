349be16e0a604a8647ae4934ea78d6e859f32436
DarkLord <you@example.com>
2022-12-26 01:18:28 +0530

    First Tetramino - Filter Rules apply problem

--- app.js
+++ app.js
@@ -7,5 +7,58 @@ document.addEventListener('DOMContentLoaded', () => {
     const StartBtn = document.querySelector('#start-button');
 
     //Tetrominoes
-    const ltetramino = [[], []];
+    const ltetramino = [
+        [1, 1 + w, 1 + 2 * w, 2],
+        [w, w + 1, w + 2, 2 * w + 2],
+        [2 * w, 1, 1 + w, 1 + 2 * w],
+        [w, 2 * w, 2 * w + 1, 2 * w + 2],
+    ];
+
+    const ztetramino = [
+        [2 * w, 1 + w, 1 + 2 * w, 2 + w],
+        [0, w, 1 + w, 1 + 2 * w],
+        [2 * w, 1 + w, 1 + 2 * w, 2 + w],
+        [0, w, 1 + w, 1 + 2 * w],
+    ];
+
+    const ttetramino = [
+        [1, 1 + w, w, 2 + w],
+        [1, 1 + w, 1 + 2 * w, 2 + w],
+        [w, 1 + w, 2 + w, 1 + 2 * w],
+        [1, 1 + w, 1 + 2 * w, w],
+    ];
+
+    const otetramino = [
+        [0, 1, w, 1 + w],
+        [(0, 1, w, 1 + w)],
+        [(0, 1, w, 1 + w)],
+        [(0, 1, w, 1 + w)],
+    ];
+
+    const itetramino = [
+        [1, 1 + w, 1 + 2 * w, 1 + 3 * w],
+        [w, 1 + w, 2 + w, 3 + w],
+        [1, 1 + w, 1 + 2 * w, 1 + 3 * w],
+        [w, 1 + w, 2 + w, 3 + w],
+    ];
+
+    const tetraminoes = [
+        ltetramino,
+        ztetramino,
+        ttetramino,
+        otetramino,
+        itetramino,
+    ];
+
+    let curPos = 4;
+    let cur = tetraminoes[0][0];
+    console.log(cur);
+    function draw() {
+        cur.forEach((index) => {
+            squares[curPos + index].classList.add('tetramino');
+            console.log(squares[curPos + index]);
+        });
+    }
+
+    draw();
 });
