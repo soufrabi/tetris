2ddf747e54b5a56fa23abad02981833bfe289f8f
DarkLord <you@example.com>
2022-12-25 23:56:45 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    let squares = Array.from(document.querySelectorAll('.grid div'));
+    const w = 10;
+    console.log(squares);
+    const ScoreDisplay = document.querySelector('#score');
+    const StartBtn = document.querySelector('#start-button');
+
+    //Tetrominoes
+    const ltetramino = [[], []];
+});
