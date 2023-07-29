98165e9578c1701044279e828a5ae2cbc2edd4ce
Anirban Dey <anirbandey@soufrabi.com>
2023-07-29 11:41:47 +0530

    Move Left,Right,Down and Rotate

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ScoreDisplay = document.querySelector('#score');
     const StartBtn = document.querySelector('#start-button');
 
+    const colBase = 'yellow';
     //Tetrominoes
     const ltetramino = [
         [1, 1 + w, 1 + 2 * w, 2],
@@ -51,15 +52,134 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     let curPos = 4;
-    let cur = tetraminoes[0][0];
-    console.log(cur);
+    let curRot = 1;
+    let curTet = 0;
+    let cur = tetraminoes[curTet][curRot];
+    // console.log(cur);
     function draw() {
+        cur = tetraminoes[curTet][curRot];
         cur.forEach((index) => {
             squares[curPos + index].classList.add('tetramino');
             squares[curPos + index].style.backgroundColor = 'red';
-            console.log(squares[curPos + index]);
+            // console.log(squares[curPos + index]);
         });
     }
+    function undraw() {
+        cur = tetraminoes[curTet][curRot];
 
+        cur.forEach((index) => {
+            squares[curPos + index].classList.remove('tetramino');
+            squares[curPos + index].style.backgroundColor = colBase;
+            // console.log(squares[curPos + index]);
+        });
+    }
     draw();
+    undraw();
+    draw();
+    function control(e) {
+        if (e.key == 'ArrowLeft') {
+            moveLeft();
+        } else if (e.key == 'ArrowRight') {
+            moveRight();
+        } else if (e.key == 'ArrowDown') {
+            moveDown();
+        } else if (e.key == 'ArrowUp') {
+            rotate();
+        }
+    }
+    document.addEventListener('keydown', control);
+    function moveLeft() {
+        const isAtLeftEdge = cur.some((index) => {
+            return (curPos + index) % w === 0;
+        });
+
+        if (isAtLeftEdge) return;
+
+        const isLeftTaken = cur.some((index) => {
+            return squares[curPos - 1 + index].classList.contains('taken');
+        });
+
+        if (isLeftTaken) return;
+
+        undraw();
+        curPos -= 1;
+        draw();
+    }
+
+    function moveRight() {
+        const isAtRightEdge = cur.some((index) => {
+            return (curPos + index) % w === w - 1;
+        });
+
+        if (isAtRightEdge) return;
+
+        const isRightTaken = cur.some((index) => {
+            return squares[curPos + 1 + index].classList.contains('taken');
+        });
+
+        if (isRightTaken) return;
+
+        undraw();
+        curPos += 1;
+        draw();
+    }
+    function moveDown() {
+        const n = squares.length;
+        const isAtBottomEdge = cur.some((index) => {
+            return curPos + index <= n - 1 && curPos + index >= n - w;
+        });
+
+        if (isAtBottomEdge) return;
+
+        const isBottomTaken = cur.some((index) => {
+            return squares[curPos + w + index].classList.contains('taken');
+        });
+
+        if (isBottomTaken) return;
+
+        undraw();
+        curPos += w;
+        draw();
+    }
+    function rotate() {
+        // const isAtLeftEdge = cur.some((index) => {
+        //     return (curPos + index) % w === 0;
+        // });
+
+        // if (isAtLeftEdge) return;
+        const n = squares.length;
+        for (let i = (curRot + 1) % 4; i != curRot; i = (i + 1) % 4) {
+            cur = tetraminoes[curTet][i];
+            const rotateIsNotValid = cur.some((index) => {
+                return curPos + index >= n || curPos + index < 0;
+            });
+            if (rotateIsNotValid) continue;
+            const rotateIsTaken = cur.some((index) => {
+                return squares[curPos + index].classList.contains('taken');
+            });
+            const isAtLeftEdge = cur.some((index) => {
+                return (curPos + index) % w === 0;
+            });
+            const isAtRightEdge = cur.some((index) => {
+                return (curPos + index) % w === w - 1;
+            });
+
+            const rotateIsPossible =
+                !rotateIsNotValid &&
+                !rotateIsTaken &&
+                !(isAtLeftEdge && isAtRightEdge);
+
+            if (rotateIsPossible) {
+                undraw();
+                curRot = i % 4;
+                draw();
+
+                return;
+            }
+        }
+
+        cur = tetraminoes[curTet][curRot];
+
+        // if (isLeftTaken) return;
+    }
 });
